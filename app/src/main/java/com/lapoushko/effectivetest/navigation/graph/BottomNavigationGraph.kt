9ca31d88f125248021e454e9f4ae00ce3b56b128
Lapoushko <vadim.dovgiy@mail.ru>
2025-02-07 14:21:23 +0500

    add navigation

--- app/src/main/java/com/lapoushko/effectivetest/navigation/graph/BottomNavigationGraph.kt
+++ app/src/main/java/com/lapoushko/effectivetest/navigation/graph/BottomNavigationGraph.kt
@@ -4,12 +4,21 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
+import com.lapoushko.effectivetest.detail.VacancyDetailScreen
 import com.lapoushko.effectivetest.favourite.FavouriteScreen
 import com.lapoushko.effectivetest.main.MainScreen
+import com.lapoushko.effectivetest.main.MainScreenHandler
 import com.lapoushko.effectivetest.message.MessageScreen
+import com.lapoushko.effectivetest.model.VacancyItem
+import com.lapoushko.effectivetest.navigation.model.Screen
 import com.lapoushko.effectivetest.navigation.model.ScreenBar
 import com.lapoushko.effectivetest.profile.ProfileScreen
 import com.lapoushko.effectivetest.respond.RespondScreen
+import com.lapoushko.effectivetest.selection.SelectionScreen
+import com.lapoushko.effectivetest.selection.SelectionScreenHandler
+import com.lapoushko.effectivetest.util.VacancyNavType
+import kotlin.reflect.typeOf
 
 /**
  * @author Lapoushko
@@ -22,7 +31,9 @@ fun BottomNavigationGraph(navController: NavHostController){
         startDestination = ScreenBar.Main.route
     ){
         composable(route = ScreenBar.Main.route) {
-            MainScreen()
+            MainScreen(
+                handler = MainScreenHandler(navController),
+            )
         }
         composable(route = ScreenBar.Favourite.route) {
             FavouriteScreen()
@@ -35,6 +46,17 @@ fun BottomNavigationGraph(navController: NavHostController){
         }
         composable(route = ScreenBar.Profile.route) {
             ProfileScreen()
+        }
+        composable<Screen.Selection> {
+            SelectionScreen(
+                handler = SelectionScreenHandler(navController),
+            )
+        }
+        composable<Screen.VacancyDetail>(
+            typeMap = mapOf(typeOf<VacancyItem>() to VacancyNavType)
+        ) { backStackEntry ->
+            val vacancy = backStackEntry.toRoute<Screen.VacancyDetail>()
+            VacancyDetailScreen(vacancy.vacancy)
         }
     }
 }
